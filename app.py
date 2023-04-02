1c96f7239259c602331f3dabf149253040f749b2
ghellardev <r.ghelbere@gmail.com>
2023-04-02 10:00:08 +0300

    Added NLP based on Latent Dirichlet Allocation (LDA) to get theme of poem

--- app.py
+++ app.py
@@ -32,7 +32,10 @@
 # if __name__ == '__main__':
 #     app.run()
 import os
-
+import nltk
+from nltk.corpus import stopwords
+from gensim import corpora, models
+import re
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 from gtts import gTTS
@@ -41,9 +44,8 @@ app = Flask(__name__)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 result = ' '
-
+matches = ''
 
 @app.route("/", methods=("GET", "POST"))
 def index():
@@ -58,11 +60,37 @@ def index():
         )
         global result
         result = response.choices[0].text
+        # Tokenize the poem and remove punctuation marks
+        tokens = nltk.word_tokenize(result)
+        tokens = [token for token in tokens if token.isalpha()]
+
+        # Remove stop words
+        stop_words = set(stopwords.words('english'))
+        tokens = [token for token in tokens if token.lower() not in stop_words]
+
+        # Create a bag-of-words representation
+        dictionary = corpora.Dictionary([tokens])
+        corpus = [dictionary.doc2bow(tokens)]
+
+        # Build the LDA model
+        lda_model = models.LdaModel(corpus=corpus,
+                                    id2word=dictionary,
+                                    num_topics=1,
+                                    passes=50,
+                                    alpha='auto',
+                                    per_word_topics=True)
+
+        # Extract the themes
+        theme = lda_model.show_topics()[0][1]
+        pattern = r'"([^"]*)"'
+        global matches
+        matches = re.findall(pattern, theme)
+        print(matches)
         myobj = gTTS(text=result, lang='en', slow=False)
         myobj.save("static/poem.mp3")
         return redirect(url_for("index", result=result))
     response2 = openai.Image.create(
-        prompt=result,
+        prompt="low quality image with following themes:\n" + str(matches),
         n=1,
         size="256x256"
     )
